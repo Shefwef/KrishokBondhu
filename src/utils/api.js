4c5182c3dc1ad1d32d1b83aa5cc5c0d8488b3292
navid-kamal <navidkamal@iut-dhaka.edu>
2024-11-06 09:58:38 +0600

    plant disease detection implemented

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+// utils/api.js
+import axios from 'axios';
+
+export async function predictDisease(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+        const response = await axios.post('http://localhost:8000/predict', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Prediction error:", error);
+        return null;
+    }
+}
